2f71c68a8afbf37503c86166ab9ca7df5467c634
Daichi Hayashi <daichi.hayashi.s5@dc.tohoku.ac.jp>
2019-10-03 14:36:58 +0900

    added mass decreation eqation model

--- trajectory.f90
+++ trajectory.f90
@@ -10,15 +10,20 @@ module mod_constant
   double precision :: const_gravity = 6.67430d-11 ! constant of gravitation [m^3/kg*s^2]
   double precision :: mass_earth = 5.9736d24 ! mass of earth (NASA) [kg]
   double precision :: radius_earth = 6.371d6 ! radius of the Earth (on red road) [m]
+  double precision :: shape = 2.0d0/3.0d0 ! for sphere (0.66666...)
+  double precision :: abl_heat = 1.0d6 ! heat for ablation [J/kg]
+  double precision :: heat_coeff = 1.0d0
 end module mod_constant
 
 module mod_parm
   ! module for parameters
   implicit none
-  double precision dt,tf,star_density,injection_speed,injection_angle,star_diameter,projected_area,mass_star
+  double precision dt,tf,star_density,injection_speed,injection_angle,d_init,area_init,m_init
+  double precision drag_coeff_inp,heat_coeff_inp
   double precision SEC,ALT,GLAT,GLONG,STL
-  double precision :: F107A, F107 = 150.
-  double precision :: AP = 4.
+  double precision :: F107A = 150.0d0
+  double precision :: F107  = 150.0d0
+  double precision :: AP = 4.0d0
   integer IYD,MASS
 end module mod_parm
 
@@ -61,14 +66,14 @@ module mod_coeff
   implicit none
 contains
   ! C_d model (Henderson)
-  function drag_coeff(h,velo)
+  function drag_coeff(h,velo,m)
     use mod_nrlmsise00
     implicit none
-    double precision h,velo,gamma,temp_w,a1,b1,c1,sa,drag_coeff
+    double precision h,velo,m,gamma,temp_w,a1,b1,c1,sa,drag_coeff
     gamma = 1.4d0 ! gas constant
     temp_w = 3.0d3 ! wall temperature [K]
     a1 = 0.9d0 + 0.34d0/mach_num(h,velo)**2
-    b1 = 1.86d0*dsqrt(mach_num(h,velo)/reynolds(h,velo))
+    b1 = 1.86d0*dsqrt(mach_num(h,velo)/reynolds(h,velo,m))
     sa = mach_num(h,velo)*dsqrt(gamma/2.0d0)
     c1 = 2.0d0 + 2.0d0/sa**2 + 1.058d0/sa*dsqrt(temp_w/temp(h)) - 2.0d0/sa**4
 
@@ -85,12 +90,11 @@ contains
     mach_num = velo/ss
   end function mach_num
   ! Reynolds number
-  function reynolds(h,velo)
-    use mod_parm, only: star_diameter
+  function reynolds(h,velo,m)
     use mod_nrlmsise00
     implicit none
-    double precision h,reynolds,velo
-    reynolds = rho(h)*velo*star_diameter/mu(h)
+    double precision h,reynolds,velo,m,diameter
+    reynolds = rho(h)*velo*diameter(m)/mu(h)
   end function reynolds
   ! viscosity coefficient
   function mu(h)
@@ -108,23 +112,24 @@ end module mod_coeff
 
 program trajectory
   use mod_constant
-  use mod_parm, only: injection_speed,injection_angle,dt,tf,ALT
+  use mod_parm, only: injection_speed,injection_angle,dt,tf,ALT,m_init
   use mod_nrlmsise00
   use mod_coeff
   implicit none
-  double precision t,r,dr,th,dth
-  double precision k1(4),k2(4),k3(4),k4(4)
-  double precision func1, func2, func3, func4, velo
+  double precision t,r,dr,th,dth,m
+  double precision k1(5),k2(5),k3(5),k4(5)
+  double precision func1,func2,func3,func4,func5,velo,area,diameter
 
   ! read run_time parameter.
   call parm
 
   ! initialize
   t = 0.0d0 ! initial time [s]
-  r = dble(ALT) + radius_earth ! initial r [m]
+  r = ALT + radius_earth ! initial r [m]
   th = 0.0d0 ! initial theta [rad.]
   dr = injection_speed*dsin(injection_angle)
   dth = injection_speed*dcos(injection_angle)/r ! V_th = r*dth
+  m = m_init
 
   ! output data
   open(100, file='./out.dat')
@@ -136,44 +141,48 @@ program trajectory
   write(102,*) '  mach     Cd'
   write(103,*) ' Altitude    density        temp        mu'
 
-  do while ((t < tf).and.((r-radius_earth) > 0.0d0))
+  do while ((t < tf).and.((r-radius_earth) > 0.0d0).and.(m > 0.0d0))
     ! outputs
-    write(100,200) t,(r-radius_earth),velo(r,dr,dth)
-    write(101,201) (r-radius_earth),mach_num(r-radius_earth,velo(r,dr,dth)),reynolds(r-radius_earth,velo(r,dr,dth)),drag_coeff(r-radius_earth,velo(r,dr,dth))
-    write(102,202) mach_num(r-radius_earth,velo(r,dr,dth)),drag_coeff(r-radius_earth,velo(r,dr,dth))
+    write(100,200) t,(r-radius_earth),velo(r,dr,dth),m
+    write(101,201) (r-radius_earth),mach_num(r-radius_earth,velo(r,dr,dth)),reynolds(r-radius_earth,velo(r,dr,dth),m),drag_coeff(r-radius_earth,velo(r,dr,dth),m)
+    write(102,202) mach_num(r-radius_earth,velo(r,dr,dth)),drag_coeff(r-radius_earth,velo(r,dr,dth),m)
     write(103,203) (r-radius_earth),rho(r-radius_earth),temp(r-radius_earth),mu(r-radius_earth)
-    ! format
-200 format(e12.4,2(f10.2))
+200 format(e12.4,2(f10.2),e12.4)
 201 format(f10.2,f7.3,e12.4,f7.3)
 202 format(2(f7.3))
 203 format(f10.2,3(e12.4))
 
     ! 4th-order runge-ketta
-    k1(1) = dt*func1(t,r,dr,th,dth)
-    k1(2) = dt*func2(t,r,dr,th,dth)
-    k1(3) = dt*func3(t,r,dr,th,dth)
-    k1(4) = dt*func4(t,r,dr,th,dth)
+    k1(1) = dt*func1(t,r,dr,th,dth,m)
+    k1(2) = dt*func2(t,r,dr,th,dth,m)
+    k1(3) = dt*func3(t,r,dr,th,dth,m)
+    k1(4) = dt*func4(t,r,dr,th,dth,m)
+    k1(5) = dt*func5(t,r,dr,th,dth,m)
 
-    k2(1) = dt*func1(t+dt/2.0d0,r+k1(1)/2.0d0,dr+k1(2)/2.0d0,th+k1(3)/2.0d0,dth+k1(4)/2.0d0)
-    k2(2) = dt*func2(t+dt/2.0d0,r+k1(1)/2.0d0,dr+k1(2)/2.0d0,th+k1(3)/2.0d0,dth+k1(4)/2.0d0)
-    k2(3) = dt*func3(t+dt/2.0d0,r+k1(1)/2.0d0,dr+k1(2)/2.0d0,th+k1(3)/2.0d0,dth+k1(4)/2.0d0)
-    k2(4) = dt*func4(t+dt/2.0d0,r+k1(1)/2.0d0,dr+k1(2)/2.0d0,th+k1(3)/2.0d0,dth+k1(4)/2.0d0)
+    k2(1) = dt*func1(t+dt/2.0d0,r+k1(1)/2.0d0,dr+k1(2)/2.0d0,th+k1(3)/2.0d0,dth+k1(4)/2.0d0,m+k1(5)/2.0d0)
+    k2(2) = dt*func2(t+dt/2.0d0,r+k1(1)/2.0d0,dr+k1(2)/2.0d0,th+k1(3)/2.0d0,dth+k1(4)/2.0d0,m+k1(5)/2.0d0)
+    k2(3) = dt*func3(t+dt/2.0d0,r+k1(1)/2.0d0,dr+k1(2)/2.0d0,th+k1(3)/2.0d0,dth+k1(4)/2.0d0,m+k1(5)/2.0d0)
+    k2(4) = dt*func4(t+dt/2.0d0,r+k1(1)/2.0d0,dr+k1(2)/2.0d0,th+k1(3)/2.0d0,dth+k1(4)/2.0d0,m+k1(5)/2.0d0)
+    k2(5) = dt*func5(t+dt/2.0d0,r+k1(1)/2.0d0,dr+k1(2)/2.0d0,th+k1(3)/2.0d0,dth+k1(4)/2.0d0,m+k1(5)/2.0d0)
 
-    k3(1) = dt*func1(t+dt/2.0d0,r+k2(1)/2.0d0,dr+k2(2)/2.0d0,th+k2(3)/2.0d0,dth+k2(4)/2.0d0)
-    k3(2) = dt*func2(t+dt/2.0d0,r+k2(1)/2.0d0,dr+k2(2)/2.0d0,th+k2(3)/2.0d0,dth+k2(4)/2.0d0)
-    k3(3) = dt*func3(t+dt/2.0d0,r+k2(1)/2.0d0,dr+k2(2)/2.0d0,th+k2(3)/2.0d0,dth+k2(4)/2.0d0)
-    k3(4) = dt*func4(t+dt/2.0d0,r+k2(1)/2.0d0,dr+k2(2)/2.0d0,th+k2(3)/2.0d0,dth+k2(4)/2.0d0)
+    k3(1) = dt*func1(t+dt/2.0d0,r+k2(1)/2.0d0,dr+k2(2)/2.0d0,th+k2(3)/2.0d0,dth+k2(4)/2.0d0,m+k2(5)/2.0d0)
+    k3(2) = dt*func2(t+dt/2.0d0,r+k2(1)/2.0d0,dr+k2(2)/2.0d0,th+k2(3)/2.0d0,dth+k2(4)/2.0d0,m+k2(5)/2.0d0)
+    k3(3) = dt*func3(t+dt/2.0d0,r+k2(1)/2.0d0,dr+k2(2)/2.0d0,th+k2(3)/2.0d0,dth+k2(4)/2.0d0,m+k2(5)/2.0d0)
+    k3(4) = dt*func4(t+dt/2.0d0,r+k2(1)/2.0d0,dr+k2(2)/2.0d0,th+k2(3)/2.0d0,dth+k2(4)/2.0d0,m+k2(5)/2.0d0)
+    k3(5) = dt*func5(t+dt/2.0d0,r+k2(1)/2.0d0,dr+k2(2)/2.0d0,th+k2(3)/2.0d0,dth+k2(4)/2.0d0,m+k2(5)/2.0d0)
 
-    k4(1) = dt*func1(t+dt,r+k3(1),dr+k3(2),th+k3(3),dth+k3(4))
-    k4(2) = dt*func2(t+dt,r+k3(1),dr+k3(2),th+k3(3),dth+k3(4))
-    k4(3) = dt*func3(t+dt,r+k3(1),dr+k3(2),th+k3(3),dth+k3(4))
-    k4(4) = dt*func4(t+dt,r+k3(1),dr+k3(2),th+k3(3),dth+k3(4))
+    k4(1) = dt*func1(t+dt,r+k3(1),dr+k3(2),th+k3(3),dth+k3(4),m+k3(5))
+    k4(2) = dt*func2(t+dt,r+k3(1),dr+k3(2),th+k3(3),dth+k3(4),m+k3(5))
+    k4(3) = dt*func3(t+dt,r+k3(1),dr+k3(2),th+k3(3),dth+k3(4),m+k3(5))
+    k4(4) = dt*func4(t+dt,r+k3(1),dr+k3(2),th+k3(3),dth+k3(4),m+k3(5))
+    k4(5) = dt*func5(t+dt,r+k3(1),dr+k3(2),th+k3(3),dth+k3(4),m+k3(5))
 
     ! calc. next step
-    r   = r   + (k1(1) + 2.0*k2(1) + 2.0*k3(1) + k4(1))/6.0d0
-    dr  = dr  + (k1(2) + 2.0*k2(2) + 2.0*k3(2) + k4(2))/6.0d0
-    th  = th  + (k1(3) + 2.0*k2(3) + 2.0*k3(3) + k4(3))/6.0d0
-    dth = dth + (k1(4) + 2.0*k2(4) + 2.0*k3(4) + k4(4))/6.0d0
+    r   = r   + (k1(1) + 2.0d0*k2(1) + 2.0d0*k3(1) + k4(1))/6.0d0
+    dr  = dr  + (k1(2) + 2.0d0*k2(2) + 2.0d0*k3(2) + k4(2))/6.0d0
+    th  = th  + (k1(3) + 2.0d0*k2(3) + 2.0d0*k3(3) + k4(3))/6.0d0
+    dth = dth + (k1(4) + 2.0d0*k2(4) + 2.0d0*k3(4) + k4(4))/6.0d0
+    m   = m   + (k1(5) + 2.0d0*k2(5) + 2.0d0*k3(5) + k4(5))/6.0d0
 
     ! integration
     t = t + dt
@@ -191,9 +200,8 @@ end program trajectory
 ! ******************************************************************
 subroutine parm
   use mod_constant
-  use mod_parm
+  use mod_parm, only: dt,tf,drag_coeff_inp,star_density,injection_speed,injection_angle,d_init,m_init,area_init,IYD,SEC,ALT,GLAT,GLONG,STL,MASS
   implicit none
-  double precision drag_coeff_inp
 
   write(6,*) 'dt :'
   read(5,*) dt ! delta t [s]
@@ -208,7 +216,7 @@ subroutine parm
   write(6,*) 'inj_ang'
   read(5,*) injection_angle
   write(6,*) 'star_d'
-  read(5,*) star_diameter
+  read(5,*) d_init
   write(6,*) 'IYD'
   read(5,*) IYD ! Year and Days
   write(6,*) 'UTC'
@@ -219,53 +227,56 @@ subroutine parm
   read(5,*) GLAT ! geodetic latitude
   write(6,*) 'GLONG'
   read(5,*) GLONG ! geodetic longtitude
-  !GLAT = GLAT*pi/180.0d0 ! [deg.] -> [rad.]
-  !GLONG = GLONG*pi/180.0d0 ! [deg.] -> [rad.]
   write(6,*) 'MASS'
   read(5,*) MASS ! Mass Number (0:temp,48:all)
-  write(6,*) 'input complete'
 
   ! variables
   STL = SEC/3600.0d0 + GLONG/15.0d0
-  projected_area = pi/4.0d0*star_diameter**2 ! projected area [m^2]
-  mass_star = star_density*pi/6.0d0*(star_diameter**3) ! mass = density * volume [kg]
+  area_init = pi/4.0d0*d_init**2 ! initial projected area [m^2]
+  m_init = star_density*pi/6.0d0*(d_init**3) ! mass = density * volume [kg]
 end subroutine parm
 
-function func1(t,r,dr,th,dth)
+function func1(t,r,dr,th,dth,m)
   ! function for dr/dt
   implicit none
-  double precision t,r,dr,th,dth,func1
+  double precision t,r,dr,th,dth,m,func1
   func1 = dr
 end function func1
 
-function func2(t,r,dr,th,dth)
+function func2(t,r,dr,th,dth,m)
   ! function for d/dt(dr/dt)
-  use mod_constant
-  use mod_parm, only: mass_star,projected_area
+  use mod_constant, only: const_gravity,mass_earth,radius_earth
   use mod_nrlmsise00
   use mod_coeff
   implicit none
-  double precision t,r,dr,th,dth,func2,velo
-  func2 = -const_gravity*mass_earth/(r*r) - 1.0d0/(2.0d0*mass_star)*drag_coeff(r-radius_earth,velo(r,dr,dth))*projected_area*rho(r-radius_earth)*velo(r,dr,dth)*dr + r*dth**2.0d0
+  double precision t,r,dr,th,dth,m,func2,velo,area
+  func2 = -const_gravity*mass_earth/(r*r) - 1.0d0/(2.0d0*m)*drag_coeff(r-radius_earth,velo(r,dr,dth),m)*area(m)*rho(r-radius_earth)*velo(r,dr,dth)*dr + r*dth**2.0d0
 end function func2
 
-function func3(t,r,dr,th,dth)
+function func3(t,r,dr,th,dth,m)
   ! function for dth/dt
   implicit none
-  double precision t,r,dr,th,dth,func3
+  double precision t,r,dr,th,dth,m,func3
   func3 = dth
 end function func3
 
-function func4(t,r,dr,th,dth)
+function func4(t,r,dr,th,dth,m)
   ! function for d/dt(dth/dt)
-  use mod_constant
-  use mod_parm, only: mass_star,projected_area
+  use mod_constant, only: radius_earth
   use mod_nrlmsise00
   use mod_coeff
   implicit none
-  double precision t,r,dr,th,dth,func4,velo
-  func4 = -1.0d0/(2.0d0*mass_star*r)*drag_coeff(r-radius_earth,velo(r,dr,dth))*projected_area*rho(r-radius_earth)*velo(r,dr,dth)*r*dth - 2.0d0/r*dr*dth
+  double precision t,r,dr,th,dth,m,func4,velo,area
+  func4 = -1.0d0/(2.0d0*m*r)*drag_coeff(r-radius_earth,velo(r,dr,dth),m)*area(m)*rho(r-radius_earth)*velo(r,dr,dth)*r*dth - 2.0d0/r*dr*dth
 end function func4
+
+! function for dm/dt
+function func5(t,r,dr,th,dth,m)
+  use mod_constant, only: heat_coeff,radius_earth,shape,abl_heat
+  use mod_nrlmsise00
+  double precision t,r,dr,th,dth,m,func5,area
+  func5 = -0.5d0*heat_coeff*area(m)*rho(r-radius_earth)*(velo(r,dr,dth)**3)/abl_heat
+end function func5
 
 function velo(r,dr,dth)
   ! function for calculate Velosity
@@ -273,4 +284,20 @@ function velo(r,dr,dth)
   double precision r,dr,dth,velo
   velo = dsqrt(dr**2 + r**2 * dth**2)
 end function velo
+
+! function for calc. Projected Area
+function area(m)
+  use mod_constant, only: shape
+  use mod_parm, only: area_init, m_init
+  double precision m,area
+  area = area_init*((m/m_init)**shape)
+end function area
+
+! function for calc. diameter
+function diameter(m)
+  use mod_constant, only: pi
+  implicit none
+  double precision diameter,area,m
+  diameter = 2.0d0*dsqrt(area(m)/pi)
+end function diameter
 
